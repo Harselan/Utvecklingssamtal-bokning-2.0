081f2d4153877f0fdadaaac872f9a196657f8c19
Harselan <haeressel@gmail.com>
2017-08-24 16:20:28 +0200

    Fixade databasproblemet

--- views/_header.php
+++ views/_header.php
@@ -2,8 +2,6 @@
 <html lang="en">
     <head>
         <title>Harre</title>
-        <link href="<?= assets('/css/bootstrap.min.css') ?>" rel="stylesheet">
-        <link href="<?= assets('/css/main.css') ?>" rel="stylesheet">
     </head>
     <body>
     </body>

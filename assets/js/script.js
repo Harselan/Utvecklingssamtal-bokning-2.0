b8609876c191e2473cf41b56e2c5d11b47ae3b4c
Harselan <haeressel@gmail.com>
2017-09-22 10:11:12 +0200

    Fixade navigeringsfältet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 $( document ).ready( function(){
+
     $(".drop-btn").click( function(){
         $(".nav").fadeToggle( 300 );
     } );
+    $( window ).resize( function() {
+
+        if ( $( document ).width() > 1031 )
+        {
+            $(".nav").css( "display", "block" );
+        }
+        else
+        {
+            $(".nav").css( "display", "none" );
+        }
+
+    } );
+
 } );
